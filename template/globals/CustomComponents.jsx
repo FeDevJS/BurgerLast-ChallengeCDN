915597b391a5c023ee704fb567e7aadad3ddfc1b
Fprog <fnaive@hotmail.com>
2022-11-02 00:01:52 -0300

    challenge completed :)

--- template/globals/CustomComponents.jsx
+++ template/globals/CustomComponents.jsx
@@ -1,22 +1,12 @@
-
-const { useState, useEffect, createContext, useRef } = React;
-
-export function Test() {
-
-    return (
-        <h1>tests</h1>
-    )
-}
-
-export function OfcanvasEnd(props) {
+export function OfcanvasStart(props) {
     const id = props.id;
     const title = props.title;
     const BodyOffcanvas = props.body;
 
     return (
-        <div className="offcanvas offcanvas-end" id={id} aria-labelledby={id + "Label"}>
+        <div className="offcanvas offcanvas-start" id={id} aria-labelledby={id + "Label"}>
             <div className="offcanvas-header">
-                <h5 id={id + "Label"}>{title}</h5>
+                <h5 id={id + "Label"} className='side-menu-title'>{title}</h5>
                 <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div className="offcanvas-body">
@@ -25,26 +15,18 @@ export function OfcanvasEnd(props) {
         </div>
     );
 }
-export function ButtonOffcanvasEnd(props) {
+export function ButtonOffcanvasStart(props) {
     const id = props.id;
     const title = props.title;
     const BodyOffcanvas = props.body;
     const button = props.button;
     const callback = props.callback;
     return (
-        <div className='ExpensesButtons'>
-            <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target={'#' + id} aria-controls={id}>
-                <img src="https://gesprender.com/app/template/assets/img/icons/mas.svg" width="20px" /> {button}
+        <div className='openMenuButton'>
+            <button className='show-menu-btn' type="button" data-bs-toggle="offcanvas" data-bs-target={'#' + id} aria-controls={id}>
+                <span className='navbar-toggler-icon'></span> {button}
             </button>
-            <div className="offcanvas offcanvas-end" id={id} aria-labelledby={id + "Label"}>
-                <div className="offcanvas-header">
-                    <h5 id={id + "Label"}>{title}</h5>
-                    <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div className="offcanvas-body">
-                    <BodyOffcanvas callback={callback} />
-                </div>
-            </div>
+            <OfcanvasStart title={title} id={id} body={BodyOffcanvas} />
         </div>
     );
 }
--- template/modules/module/Module.jsx
+++ template/modules/module/Module.jsx
@@ -1,26 +1,134 @@
 // Import custom methods
 import { API } from "../../globals/CustomHooks";
-import { ButtonOffcanvasEnd, OfcanvasEnd } from "../../globals/CustomComponents";
+import { ButtonOffcanvasStart } from "../../globals/CustomComponents";
+import './componente.css';
 
 // React dependecies
-const { useState, useEffect, createContext, useContext, useRef } = React;
+const { useState, useEffect, Fragment } = React;
 
 const App = () => {
 
     return (
-        <div>
-            <FirstComponent />
-            <ButtonOffcanvasEnd  id='addExpense' title='Agregar nuevo gastos' button='Nuevo gasto personal' body={FirstComponent} callback={''} />
-        </div>
+        <Fragment>
+            <HeroSection />
+            <MainHeader />
+            <AccordionSection />
+        </Fragment>
     )
 }
 
-const FirstComponent = () => {
+const HeroSection = () => {
     return (
-        <h2>React CDN </h2>
-    );
-}
+        <section className='hero-layout'>
+            <div className='layer'></div>
+            <article className='hero-business-info-container'>
+                <div className='business-info-img'></div>
+                <section className='business-info-details'>
+                    <p className='business-info-place-name'>BurgerLast</p>
+                    <address className='business-info-adress'>Juan del Campillo 1413</address>
+                    <div className='business-info-icons-container'>
+                        <a 
+                            href='https://api.whatsapp.com/send/?phone=5493424416404&text&type=phone_number&app_absent=0'
+                            rel='noopener noreferrer' 
+                            target='_blank'
+                        ><img src='https://gesprender.com/app/template/assets_c/img/icons/icon_wsp_color.png' alt='Icono WhatsApp' />
+                        </a>
+                        <a 
+                            href='https://www.instagram.com/gesprender/'
+                            rel='noopener noreferrer' 
+                            target='_blank'
+                        ><img src='https://gesprender.com/app/template/assets_c/img/icons/icon_ig_color.png' alt='Icono Instagram' />
+                        </a>
 
+                    </div>
+                </section>
+            </article>
+        </section>
+    );
+};
+
+const SideMenu = () => {
+    const [categories, setCategories] = useState(null);
+    useEffect(() => {
+        API._get('https://api.gesprender.com/products/burgerlast')
+            .then(res => setCategories(res.categorys))
+    }, []);
+    return (
+        <nav>
+            <div className='list-group'>
+                {categories && categories.map((category, i) => (
+                    <NavItem title={category} key={i} />
+                ))}
+            </div>
+        </nav>
+    );
+};
+const NavItem = ({title}) => {
+    return (
+        <a href={`#${title}-category`}><button className='list-group-item list-group-item-action'>{title}</button></a>
+    );
+};
+
+const MainHeader = () => {
+    return (
+        <section className='navbar navbar-dark bg-dark main-header-container'>
+            <article className='container d-flex'>
+                <ButtonOffcanvasStart  id='sideMenuTitle' title='Categorías de productos' button='Categorías' body={SideMenu} />
+            </article>
+        </section>
+    );
+};
+
+const AccordionSection = () => {
+    const [categories, setCategories] = useState(null);
+    useEffect(() => {
+        API._get('https://api.gesprender.com/products/burgerlast')
+            .then(res => setCategories(res.categorys))
+    }, []);
+    return (
+        <Fragment>
+            {categories && categories.map((category, i) => (
+                <Accordion title={category} key={i} />
+            ))}
+        </Fragment>
+    );
+};
+
+const Accordion = ({title}) => {
+    const [items, setItems] = useState(null);
+    useEffect(() => {
+        API._get('https://api.gesprender.com/products/burgerlast')
+            .then(res => setItems(res.products[title]))
+    }, []);
+    return (
+        <section id={`${title}-category`} className='container accordion-item accordion-container'>
+            <div className='accordion-header accordion-collapsible'>
+                <button type='button' className='accordion-button' data-bs-toggle="collapse" data-bs-target={`#${title}`} aria-expanded="true" aria-controls={title}>{title}</button>
+            </div>
+            <ul id={title} className='accordion-items-container accordion-collapse collapse show'>
+                {items && items.map(item => (
+                    <AccordionItem 
+                        key={item.id} 
+                        title={item.product}
+                        description={item.description}
+                        price={item.price_sale} />
+                ))}
+            </ul>
+        </section>
+    );
+};
+
+const AccordionItem = ({title, description, price}) => {
+    return (
+        <li className='d-flex justify-content-between list-group-item accordion-items'>
+            <div>
+                <p className='accordion-item-title'>{title}</p>
+                <p className='accordion-item-description'>{description}</p>
+            </div>
+            <p className='accordion-item-price'>${price}</p>
+        </li>
+    );
+};
 
 // Rendering
 const module = document.querySelector('#modulo');
